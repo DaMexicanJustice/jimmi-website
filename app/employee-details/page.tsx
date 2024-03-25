2e3007c8b0e9c21da6e8b7205689c7584789c1d2
DaMexicanJustice <mrrichtofen@gmail.com>
2024-03-25 09:13:28 +0100

    suspense added trying to fix build error

--- app/employee-details/page.tsx
+++ app/employee-details/page.tsx
@@ -2,7 +2,7 @@
 import Footer from "../ui/footer";
 import Navbar from "../ui/navbar";
 import Image from "next/image";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import {
   EnvelopeIcon,
@@ -99,149 +99,150 @@ export default function EmployeeDetails() {
   return (
     <>
       <Navbar></Navbar>
-      <section
-        id="Employee-details"
-        className="px-5 min-h-[calc(100svh-6rem)] mt-24 bg-slate-300 py-5
+      <Suspense>
+        <section
+          id="Employee-details"
+          className="px-5 min-h-[calc(100svh-6rem)] mt-24 bg-slate-300 py-5
       md:px-20"
-      >
-        <main
-          className="flex flex-col h-full gap-5 z-40 font-conduit text-center
-        md:gap-10"
         >
-          <div
-            className="flex flex-col gap-10 bg-slate-100 p-5
-          md:gap-12 md:flex-row"
+          <main
+            className="flex flex-col h-full gap-5 z-40 font-conduit text-center
+        md:gap-10"
           >
-            {/* Employee name: {data && <pre>{data.employees[id].name}</pre>} */}
             <div
-              className="flex flex-col
-            md:self-start md:gap-5"
+              className="flex flex-col gap-10 bg-slate-100 p-5
+          md:gap-12 md:flex-row"
             >
-              <p className="uppercase font-bold font-conduitbold text-2xl text-slate-700">
-                {data && data.name}
-              </p>
               <div
-                id="image-container"
-                className="w-full h-96 mb-5
-            md:h-full md:size-3/12
-            lg:size-72"
+                className="flex flex-col
+            md:self-start md:gap-5"
               >
-                {data && data.img && (
-                  <Image
-                    src={data.img}
-                    width={390}
-                    height={260}
-                    alt="Medarbejder billed"
-                    className="w-full h-full grayscale
-                  lg:size-96"
-                  />
-                )}
-              </div>
-              <div>
-                <p className="uppercase font-bold font-conduitbold text-xl text-slate-700">
+                <p className="uppercase font-bold font-conduitbold text-2xl text-slate-700">
                   {data && data.name}
                 </p>
-                <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                  {data && data.role}
-                </p>
-              </div>
-              <div>
-                <div className="flex flex-row gap-2 justify-center items-center">
-                  <EnvelopeIcon className="text-slate-900 w-5"></EnvelopeIcon>
-                  <p className="font-conduit text-slate-800">
-                    {data && data.email}
-                  </p>
+                <div
+                  id="image-container"
+                  className="w-full h-96 mb-5
+            md:h-full md:size-3/12
+            lg:size-72"
+                >
+                  {data && data.img && (
+                    <Image
+                      src={data.img}
+                      width={390}
+                      height={260}
+                      alt="Medarbejder billed"
+                      className="w-full h-full grayscale
+                  lg:size-96"
+                    />
+                  )}
                 </div>
-                <div className="flex flex-row gap-2 justify-center items-center">
-                  <PhoneIcon className="text-slate-900 w-5"></PhoneIcon>
-                  <a
-                    href={`tel:${data && data.phone}`}
-                    className="font-conduit text-slate-800"
-                  >
-                    {data && data.phone}
-                  </a>
-                </div>
-              </div>
-              <div>
                 <div>
                   <p className="uppercase font-bold font-conduitbold text-xl text-slate-700">
-                    Kompetence dækning
+                    {data && data.name}
                   </p>
                   <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                    Sprog
+                    {data && data.role}
                   </p>
-                  <p className="text-slate-900">
-                    {data && data.competences.languages.Danish}
-                  </p>
-                  <p className="text-slate-900">
-                    {data && data.competences.languages.English}
-                  </p>
-                  <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                    Uddannelse/Kurser
-                  </p>
-                  {data && data.competences.education && (
-                    <div>
-                      {Object.keys(data.competences.education).map(
-                        (key: string, index: number) => (
-                          <p className="text-slate-900" key={index}>
-                            {data.competences.education[key]}
-                          </p>
-                        )
-                      )}
-                    </div>
-                  )}
-                  <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                    Erfaring
-                  </p>
-                  {data && data.competences.experience && (
-                    <div>
-                      {Object.keys(data.competences.experience).map(
-                        (key: string, index: number) => (
-                          <p className="text-slate-900" key={index}>
-                            {data.competences.experience[key]}
-                          </p>
-                        )
-                      )}
-                    </div>
-                  )}
+                </div>
+                <div>
+                  <div className="flex flex-row gap-2 justify-center items-center">
+                    <EnvelopeIcon className="text-slate-900 w-5"></EnvelopeIcon>
+                    <p className="font-conduit text-slate-800">
+                      {data && data.email}
+                    </p>
+                  </div>
+                  <div className="flex flex-row gap-2 justify-center items-center">
+                    <PhoneIcon className="text-slate-900 w-5"></PhoneIcon>
+                    <a
+                      href={`tel:${data && data.phone}`}
+                      className="font-conduit text-slate-800"
+                    >
+                      {data && data.phone}
+                    </a>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <p className="uppercase font-bold font-conduitbold text-xl text-slate-700">
+                      Kompetence dækning
+                    </p>
+                    <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
+                      Sprog
+                    </p>
+                    <p className="text-slate-900">
+                      {data && data.competences.languages.Danish}
+                    </p>
+                    <p className="text-slate-900">
+                      {data && data.competences.languages.English}
+                    </p>
+                    <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
+                      Uddannelse/Kurser
+                    </p>
+                    {data && data.competences.education && (
+                      <div>
+                        {Object.keys(data.competences.education).map(
+                          (key: string, index: number) => (
+                            <p className="text-slate-900" key={index}>
+                              {data.competences.education[key]}
+                            </p>
+                          )
+                        )}
+                      </div>
+                    )}
+                    <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
+                      Erfaring
+                    </p>
+                    {data && data.competences.experience && (
+                      <div>
+                        {Object.keys(data.competences.experience).map(
+                          (key: string, index: number) => (
+                            <p className="text-slate-900" key={index}>
+                              {data.competences.experience[key]}
+                            </p>
+                          )
+                        )}
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
+              <div className="flex flex-col gap-5">
+                <hr className="border border-solid border-slate-900"></hr>
+                <p className="text-blue-950 text-xl text-justify">
+                  {data && data.competences.article.headline}
+                </p>
+                <hr className="border border-solid border-slate-900"></hr>
+                <p className="text-justify text-slate-900 whitespace-pre-line">
+                  {data && data.competences.article.text}
+                </p>
+                <p className="uppercase font-conduitbold text-xl text-blue-950">
+                  Hvilke borgere er jeg god til
+                </p>
+                <p className="text-justify text-slate-900">
+                  {data && data.competences["citizen-type"]}
+                </p>
+                <p className="text-justify text-slate-950 text-lg">
+                  {data && data.competences.resume}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col gap-5">
-              <hr className="border border-solid border-slate-900"></hr>
-              <p className="text-blue-950 text-xl text-justify">
-                {data && data.competences.article.headline}
-              </p>
-              <hr className="border border-solid border-slate-900"></hr>
-              <p className="text-justify text-slate-900 whitespace-pre-line">
-                {data && data.competences.article.text}
-              </p>
-              <p className="uppercase font-conduitbold text-xl text-blue-950">
-                Hvilke borgere er jeg god til
-              </p>
-              <p className="text-justify text-slate-900">
-                {data && data.competences["citizen-type"]}
-              </p>
-              <p className="text-justify text-slate-950 text-lg">
-                {data && data.competences.resume}
-              </p>
-            </div>
-          </div>
-          <ThemeProvider theme={theme}>
-            <Link href="/employees">
-              <Button
-                className="p-3 uppercase font-bold text-xl bg-yellow-500
+            <ThemeProvider theme={theme}>
+              <Link href="/employees">
+                <Button
+                  className="p-3 uppercase font-bold text-xl bg-yellow-500
                   md:text-sm md:p-2
                   xl:hidden"
-                variant="contained"
-                color="ochre"
-              >
-                Tilbage Til Oversigt
-              </Button>
-            </Link>
-          </ThemeProvider>
-        </main>
-      </section>
+                  variant="contained"
+                  color="ochre"
+                >
+                  Tilbage Til Oversigt
+                </Button>
+              </Link>
+            </ThemeProvider>
+          </main>
+        </section>
+      </Suspense>
       <Footer></Footer>
     </>
   );
