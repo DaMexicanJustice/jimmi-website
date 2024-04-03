998b0187631a49ad2950817f233f0aa63e6592d9
damexicanjustice <mrrichtofen@gmail.com>
2024-04-03 09:34:14 +0200

    fixed employee details. New approach using client fetching and null validation

--- app/employee-details/page.tsx
+++ app/employee-details/page.tsx
@@ -70,10 +70,12 @@ interface Employee {
 }
 
 export default function EmployeeDetails() {
-  /* const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
   const search = searchParams.get("id");
-  const id = parseInt(search || "0", 10); //
-  const [data, setData] = useState<Employee | null>(null);
+  const id = parseInt(search || "0", 10);
+
+  const [data, setData] = useState<null | Employee>(null);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     // If you're using Create React App and the file is in the public folder
@@ -86,6 +88,7 @@ export default function EmployeeDetails() {
       })
       .then((data) => {
         setData(data.employees[id]);
+        setLoading(false);
         //console.log(data.employees[id].name);
       })
       .catch((error) =>
@@ -94,92 +97,85 @@ export default function EmployeeDetails() {
           error
         )
       );
-  }, [id]); */
+  }, [id]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
 
   return (
     <>
       <Navbar></Navbar>
-      {/* <section
-          id="Employee-details"
-          className="px-5 min-h-[calc(100svh-6rem)] mt-24 bg-slate-300 py-5
+      <section
+        id="Employee-details"
+        className="px-5 min-h-[calc(100svh-6rem)] mt-24 bg-slate-300 py-5
       md:px-20"
-        >
-          <main
-            className="flex flex-col h-full gap-5 z-40 font-conduit text-center
+      >
+        <main
+          className="flex flex-col h-full gap-5 z-40 font-conduit text-center
         md:gap-10"
+        >
+          <div
+            className="flex flex-col gap-10 bg-slate-100 p-5
+          md:gap-12 md:flex-row"
           >
             <div
-              className="flex flex-col gap-10 bg-slate-100 p-5
-          md:gap-12 md:flex-row"
-            >
-              <div
-                className="flex flex-col
+              className="flex flex-col
             md:self-start md:gap-5"
-              >
-                <p className="uppercase font-bold font-conduitbold text-2xl text-slate-700">
-                  {data && data.name}
-                </p>
-                <div
-                  id="image-container"
-                  className="w-full h-96 mb-5
+            >
+              <p className="uppercase font-bold font-conduitbold text-2xl text-slate-700"></p>
+              <div
+                id="image-container"
+                className="w-full h-96 mb-5
             md:h-full md:size-3/12
             lg:size-72"
-                >
-                  {data && data.img && (
-                    <Image
-                      src={data.img}
-                      width={390}
-                      height={260}
-                      alt="Medarbejder billed"
-                      className="w-full h-full grayscale
+              >
+                <Image
+                  src="/images/jimmi.jpg"
+                  width={390}
+                  height={260}
+                  alt="Medarbejder billed"
+                  className="w-full h-full grayscale
                   lg:size-96"
-                    />
-                  )}
+                />
+              </div>
+              <div>
+                <p className="uppercase font-bold font-conduitbold text-xl text-slate-700">
+                  {data ? data.name : "no-data"}
+                </p>
+                <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
+                  role
+                </p>
+              </div>
+              <div>
+                <div className="flex flex-row gap-2 justify-center items-center">
+                  <EnvelopeIcon className="text-slate-900 w-5"></EnvelopeIcon>
+                  <p className="font-conduit text-slate-800">email</p>
                 </div>
+                <div className="flex flex-row gap-2 justify-center items-center">
+                  <PhoneIcon className="text-slate-900 w-5"></PhoneIcon>
+                  <a
+                    href="tel:4512345678"
+                    className="font-conduit text-slate-800"
+                  >
+                    phone
+                  </a>
+                </div>
+              </div>
+              <div>
                 <div>
                   <p className="uppercase font-bold font-conduitbold text-xl text-slate-700">
-                    {data && data.name}
+                    Kompetence dækning
                   </p>
                   <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                    {data && data.role}
+                    Sprog
                   </p>
-                </div>
-                <div>
-                  <div className="flex flex-row gap-2 justify-center items-center">
-                    <EnvelopeIcon className="text-slate-900 w-5"></EnvelopeIcon>
-                    <p className="font-conduit text-slate-800">
-                      {data && data.email}
-                    </p>
-                  </div>
-                  <div className="flex flex-row gap-2 justify-center items-center">
-                    <PhoneIcon className="text-slate-900 w-5"></PhoneIcon>
-                    <a
-                      href={`tel:${data && data.phone}`}
-                      className="font-conduit text-slate-800"
-                    >
-                      {data && data.phone}
-                    </a>
-                  </div>
-                </div>
-                <div>
-                  <div>
-                    <p className="uppercase font-bold font-conduitbold text-xl text-slate-700">
-                      Kompetence dækning
-                    </p>
-                    <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                      Sprog
-                    </p>
-                    <p className="text-slate-900">
-                      {data && data.competences.languages.Danish}
-                    </p>
-                    <p className="text-slate-900">
-                      {data && data.competences.languages.English}
-                    </p>
-                    <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                      Uddannelse/Kurser
-                    </p>
-                    {data && data.competences.education && (
-                      <div>
+                  <p className="text-slate-900">Sprog</p>
+                  <p className="text-slate-900">Sprog</p>
+                  <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
+                    Uddannelse/Kurser
+                  </p>
+                  uddannelse <br></br> kursus
+                  {/* <div>
                         {Object.keys(data.competences.education).map(
                           (key: string, index: number) => (
                             <p className="text-slate-900" key={index}>
@@ -187,13 +183,12 @@ export default function EmployeeDetails() {
                             </p>
                           )
                         )}
-                      </div>
-                    )}
-                    <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
-                      Erfaring
-                    </p>
-                    {data && data.competences.experience && (
-                      <div>
+                      </div> */}
+                  <p className="uppercase font-bold font-conduitbold text-lg text-orange-500">
+                    Erfaring
+                  </p>
+                  Erfaring 1<br></br> erfaring 2
+                  {/* <div>
                         {Object.keys(data.competences.experience).map(
                           (key: string, index: number) => (
                             <p className="text-slate-900" key={index}>
@@ -201,46 +196,39 @@ export default function EmployeeDetails() {
                             </p>
                           )
                         )}
-                      </div>
-                    )}
-                  </div>
+                      </div> */}
                 </div>
               </div>
-              <div className="flex flex-col gap-5">
-                <hr className="border border-solid border-slate-900"></hr>
-                <p className="text-blue-950 text-xl text-justify">
-                  {data && data.competences.article.headline}
-                </p>
-                <hr className="border border-solid border-slate-900"></hr>
-                <p className="text-justify text-slate-900 whitespace-pre-line">
-                  {data && data.competences.article.text}
-                </p>
-                <p className="uppercase font-conduitbold text-xl text-blue-950">
-                  Hvilke borgere er jeg god til
-                </p>
-                <p className="text-justify text-slate-900">
-                  {data && data.competences["citizen-type"]}
-                </p>
-                <p className="text-justify text-slate-950 text-lg">
-                  {data && data.competences.resume}
-                </p>
-              </div>
             </div>
-            <ThemeProvider theme={theme}>
-              <Link href="/employees">
-                <Button
-                  className="p-3 uppercase font-bold text-xl bg-yellow-500
+            <div className="flex flex-col gap-5">
+              <hr className="border border-solid border-slate-900"></hr>
+              <p className="text-blue-950 text-xl text-justify">Headline</p>
+              <hr className="border border-solid border-slate-900"></hr>
+              <p className="text-justify text-slate-900 whitespace-pre-line">
+                Text
+              </p>
+              <p className="uppercase font-conduitbold text-xl text-blue-950">
+                Hvilke borgere er jeg god til
+              </p>
+              <p className="text-justify text-slate-900">Competence text</p>
+              <p className="text-justify text-slate-950 text-lg">Resume text</p>
+            </div>
+          </div>
+          <ThemeProvider theme={theme}>
+            <Link href="/employees">
+              <Button
+                className="p-3 uppercase font-bold text-xl bg-yellow-500
                   md:text-sm md:p-2
                   xl:hidden"
-                  variant="contained"
-                  color="ochre"
-                >
-                  Tilbage Til Oversigt
-                </Button>
-              </Link>
-            </ThemeProvider>
-          </main>
-        </section> */}
+                variant="contained"
+                color="ochre"
+              >
+                Tilbage Til Oversigt
+              </Button>
+            </Link>
+          </ThemeProvider>
+        </main>
+      </section>
       <Footer></Footer>
     </>
   );
